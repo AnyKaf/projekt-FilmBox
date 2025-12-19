3bfe19e2be4e6ce0382220fd8ddd94a08c51b7c9
Anežka Kafková <anezka@zzaba.cz>
2025-12-19 09:33:52 +0100

    hamburger bonus

--- spolecne.js
+++ spolecne.js
@@ -1,7 +1,18 @@
 const menuTlacitko = document.querySelector("#menu-tlacitko")
 const menuPolozky = document.querySelector("#menu-polozky")
+const ikona = document.querySelector("i")
+
 const click = () => {
-    menuPolozky.classList.toggle("show")
+    const jeOtevrene = menuPolozky.classList.toggle("show")
+
+    ikona.classList.remove("fa-xmark", "fa-bars")
+
+    if (jeOtevrene) {
+        ikona.classList.add("fa-xmark")
+      } else {
+        ikona.classList.add("fa-bars")
+      }
 }
 
 menuTlacitko.addEventListener("click", click)
+
